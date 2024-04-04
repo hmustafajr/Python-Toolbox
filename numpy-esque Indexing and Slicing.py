ac5ea1aee03c79916349f2feb47960c2d3090c08
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-04-04 13:25:46 -0700

    Create numpy-esque Indexing and Slicing.py

---
+++ numpy-esque Indexing and Slicing.py
@@ -0,0 +1,17 @@
+tensor = torch.ones(4, 4)
+print('First row: ',tensor[0])
+print('First column: ', tensor[:, 0])
+print('Last column:', tensor[..., -1])
+tensor[:,1] = 0
+print(tensor)
+
+'''
+Example Output
+First row:  tensor([1., 1., 1., 1.])
+First column:  tensor([1., 1., 1., 1.])
+Last column: tensor([1., 1., 1., 1.])
+tensor([[1., 0., 1., 1.],
+        [1., 0., 1., 1.],
+        [1., 0., 1., 1.],
+        [1., 0., 1., 1.]])
+'''
