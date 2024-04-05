42b6ccd86d89d74e34b9344ee2c6a74358a9ed85
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-04-04 17:32:32 -0700

    Update Arithmetic Operations.py

--- Arithmetic Operations.py
+++ Arithmetic Operations.py
@@ -1,1 +1,17 @@
 # This will compute the matrix multiplication between two tensors. All of the ys have the same value.
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+y3 = torch.rand_like(tensor)
+torch.matmul(tensor, tensor.T, out=y3)
+
+# This computes the elemental product. All of the zs will have the same value.
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+z3 = torch.rand_like(tensor)
+torh.mul(tensor, tensor, out=z3)
+
+" tensor([[1., 0., 1., 1.],
+        [1., 0., 1., 1.],
+        [1., 0., 1., 1.],
+        [1., 0., 1., 1.]])
+"
