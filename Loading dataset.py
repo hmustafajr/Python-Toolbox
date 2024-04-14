19d52165e38d398d3413d719b20c0abfc0c49f96
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-04-13 17:42:46 -0700

    Create Loading dataset.py

---
+++ Loading dataset.py
@@ -0,0 +1,20 @@
+
+%matplotlib inline
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lamda
+import matplotlib.pyplot as plt
+
+training_data = datasets.FashionMNIST (
+    root = "data:,
+    train = True,
+    download = True,
+    transform = ToTensor()
+)
+test_data = datasets.FashionMNIST(
+    root = "data"
+    train = False,
+    download = True,
+    transform = ToTensor()
+)
