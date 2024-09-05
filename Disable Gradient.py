d5de1edfc089c7ec1e925d0fb64be023a2d539c9
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-09-04 19:34:08 -0700

    Update Disable Gradient.py

--- Disable Gradient.py
+++ Disable Gradient.py
@@ -4,3 +4,8 @@ print(z.requires_grad)
 with torch.no_grad():
   z = torch.matmul(x,w)+b
 print(z.requires_grad)
+
+//Alternate Mwthod
+z = torch.matmul(x,w)+b
+z_det = z.detach()
+print(z_det.requires_grad)
